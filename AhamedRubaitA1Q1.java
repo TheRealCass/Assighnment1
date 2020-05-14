493092acbea7fe2582e47fc232209bd3d305a214
AHMRC1703 <ahmrc1703@learning.icmanitoba.ca>
2020-05-14 02:38:46 -0500

    initial commit

---
+++ AhamedRubaitA1Q1.java
@@ -0,0 +1,77 @@
+/**
+ * Class Name: AhamedShoumikA1Q1
+ * 
+ * COMP2140 Section D01
+ * Assighnment  Assightnment #1, Question #1
+ * @author   Rubait Ul Ahamed, 007876180
+ * @version (13th May 2020)
+ * 
+ * methods:
+ * Description:
+ */
+public class AhamedRubaitA1Q1 {
+
+    /**
+     * name: main function
+     * @param Strings[]
+     * @return void
+     */
+    public static void main(String[] args) {
+        
+    }
+    
+}
+
+
+
+//==============================================================================================
+
+/**
+ * Class Name: Book
+ * 
+ * COMP2140 Section D01
+ * Assighnment  Assightnment #1, Question #1
+ * @author   Rubait Ul Ahamed, 007876180
+ * @version (13th May 2020)
+ * 
+ * methods: getInitials, giveOnLoan, RecieveBook
+ * Description:
+ */
+class Book{
+
+    
+
+
+
+}
+
+
+//==============================================================================================
+
+/**
+ * Class Name: Library
+ * 
+ * COMP2140 Section D01
+ * Assighnment  Assightnment #1, Question #1
+ * @author   Rubait Ul Ahamed, 007876180
+ * @version (13th May 2020)
+ * 
+ * methods:
+ * Description:
+ */
+class Library{
+
+    
+
+    /**
+     * constructor for Library
+     * @param void
+     */
+    public Library(){
+
+    }
+
+
+
+
+}
