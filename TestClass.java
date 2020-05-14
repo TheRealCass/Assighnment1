3c4dfd3eaf66b6da4e08744278f0016a125b7203
AHMRC1703 <ahmrc1703@learning.icmanitoba.ca>
2020-05-14 03:07:07 -0500

    added testclass to test methods in books class

---
+++ TestClass.java
@@ -0,0 +1,9 @@
+public class TestClass {
+    
+    public static void main(String[] args) {
+
+        Book myFavBook = new Book("Game of Thrones", "Gorge", "Martin");
+        System.out.println(myFavBook.toString());
+    }
+    
+}
